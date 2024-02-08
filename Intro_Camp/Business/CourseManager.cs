9f0c201829a36342d95c322dc3248ca80cb5e5e9
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-02-08 22:34:19 +0300

    Proje dosyası ekle.

---
+++ Intro_Camp/Business/CourseManager.cs
@@ -0,0 +1,43 @@
+﻿using Intro_Camp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_Camp.Business;
+
+public class CourseManager
+{
+    Course[]Courses=new Course[3];
+   public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Name = "C# ";
+        course1.Description = ".Net 8";
+        course1.Id = 1;
+        course1.Price = 122.1;
+
+        Course course2 = new Course();
+        course2.Name = "Java ";
+        course2.Description = "  ";
+        course2.Id = 2;
+        course2.Price = 128.1;
+
+        Course course3 = new Course();
+        course3.Name = "php ";
+        course3.Description = " ";
+        course3.Id = 3;
+        course3.Price = 130.1;
+
+        Courses[0]=course1;
+        Courses[1]=course2;
+        Courses[2]=course3;
+        
+    }
+    public Course[] GetAll()
+    {
+        return Courses;
+    }
+}
---
+++ Intro_Camp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Intro_Camp.Business;
+using Intro_Camp.Entity;
+
+Course course1= new Course();
+course1.Name = "C# ";
+course1.Description = ".Net 8";
+course1.Id = 1;
+course1.Price = 122.1;
+
+Course course2 = new Course();
+course2.Name = "Java ";
+course2.Description = "  ";
+course2.Id = 2;
+course2.Price = 128.1;
+
+Course course3 = new Course();
+course3.Name = "php ";
+course3.Description = " ";
+course3.Id = 3;
+course3.Price = 130.1;
+
+Course[] courses = { course1, course2, course3 };
+for (int i = 0; i <courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name+"/ " + courses[i].Price);
+}
+
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 =courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++) {
+
+    Console.WriteLine(courses2[i].Id);
+
+}
